a8866b4443cab455cddb452b2017813be492f251
joe-gregory <josephedward2nd@gmail.com>
2023-04-17 13:49:14 -0700

    Add project files.

---
+++ DataLibrary/Data/IOrderData.cs
@@ -0,0 +1,12 @@
+﻿using DataLibrary.Models;
+
+namespace DataLibrary.Data
+{
+    public interface IOrderData
+    {
+        Task<int> CreateORder(OrderModel order);
+        Task<int> DeleteOrder(int orderId);
+        Task<OrderModel> GetOrderById(int orderId);
+        Task<int> UpdateOrderName(int orderId, string orderName);
+    }
+}
---
+++ TimsDinerDB/dbo/Tables/Order.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Order]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [OrderName] NCHAR(50) NOT NULL, 
+    [OrderDate] DATETIME2 NOT NULL, 
+    [FoodId] INT NOT NULL, 
+    [Quantity] INT NOT NULL, 
+    [Total] MONEY NOT NULL,
+
+)
